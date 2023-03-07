7337df4bc30e0096e358cc5d55a70acc85dd5415
maldo0548 <maldonado.miguel45@gmail.com>
2023-03-07 10:24:45 -0700

    Updated next config to include avatar ui domain.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 module.exports = {
     reactStrictMode: true,
     images: {
+        dangerouslyAllowSVG: true,
         domains: ["links.papareact.com", "lh3.googleusercontent.com", "ui-avatars.com"],
     },
     experimental: {
